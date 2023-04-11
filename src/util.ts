6ce5c44518b1fcc5d47f42cf7749b003b3881c34
Anton Winkler <antonwinkler98@web.de>
2023-04-11 22:04:01 +0200

    fix?

--- src/util.ts
+++ src/util.ts
@@ -8,18 +8,19 @@ export function SetGlobal(key: string, val: any) {
 }
 
 type EventFunc = (this: void, event: event) => void;
-const eventList: Map<defines.events | defines.events[] | string, EventFunc[]> = new Map();
+const eventList: Map<defines.events | string, EventFunc[]> = new Map();
 /**
  * Define an event. Better to use this than directly script.on_event, as this will support multiple callbacks at the same time
  * @param event event to call
  * @param callback callback function
  */
-export function DefineEvent<T extends keyof AllEvents>(event: T, callback: (this: void, event: AllEvents[T]) => void) {
+export function DefineEvent<T extends keyof allEvents>(
+	event: defines.events | (T extends string ? T : string),
+	callback: (this: void, event: allEvents[T]) => void
+) {
 	if (!eventList.has(event)) eventList.set(event, []);
 	eventList.get(event)!.push(callback as EventFunc);
 }
-
-const t = DefineEvent(defines.events.on_area_cloned, event => {});
 
 /**
  * Reload the previously defined events
