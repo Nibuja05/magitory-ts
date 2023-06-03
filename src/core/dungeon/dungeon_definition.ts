86537f6e547377e932e182d606e66cb8e5a7651e
HanniWalter <hannibali@gmx.de>
2023-06-03 14:16:26 +0200

    conors

--- src/core/dungeon/dungeon_definition.ts
+++ src/core/dungeon/dungeon_definition.ts
@@ -1,8 +1,28 @@
+interface Dungeon_Prototype {
+	type: string;
+	name: string;
+	first_room: string;
+}
+
+interface Room_Prototype {
+	type: string;
+	name: string;
+	roomtype: string;
+	layout: number[][];
+}
+
 const basedungeon = {
 	type: "dungeon",
 	name: "base_dungeon",
-	entrance: "base_entrance"
-};
+	first_room: "base_entrance"
+} as Dungeon_Prototype;
+
+//grids are here stored in a grid thus there is need for emtpy spaces
+//0 = empty will only used here
+//1 = basic room
+//2 = extendable (here can spawn floors)
+//10 = entrance from atop
+//11 = exit to the upper surface (nauvis)
 
 const baseroom = {
 	type: "room",
@@ -14,25 +34,19 @@ const baseroom = {
 		[2, 1, 1, 2],
 		[0, 2, 2, 0]
 	]
-};
+} as Room_Prototype;
 
 const baseentrance = {
 	type: "room",
 	name: "base_entrance",
 	roomtype: "entrance",
-	entrance_position: [
-		[0, 0, 0, 0],
-		[0, 1, 0, 0],
-		[0, 0, 0, 0],
-		[0, 0, 0, 0]
-	],
 	layout: [
 		[0, 0, 2, 0],
-		[0, 1, 1, 2],
-		[2, 1, 1, 2],
+		[0, 10, 1, 2],
+		[2, 1, 11, 2],
 		[0, 2, 2, 0]
 	]
-};
+} as Room_Prototype;
 
-export const dungeons = { base_dungeon: basedungeon };
-export const rooms = { base_room: baseroom, base_entrance: baseentrance };
+export const dungeon_prototypes = { base_dungeon: basedungeon } as Record<string, Dungeon_Prototype>;
+export const room_prototypes = { base_room: baseroom, base_entrance: baseentrance } as Record<string, Room_Prototype>;
--- src/core/dungeon/dungeon_main.ts
+++ src/core/dungeon/dungeon_main.ts
@@ -1,6 +1,6 @@
 import { DefineEvent, overlappingAreas } from "../../util";
 import { OneWayTeleport } from "../../util";
-import { dungeons, rooms } from "./dungeon_definition";
+import { dungeon_prototypes, room_prototypes } from "./dungeon_definition";
 class Room {
 	constructor() {}
 }
@@ -11,24 +11,46 @@ class Room {
 //grids are stored in a sparse matrice
 //eception for the grids in the dungeon_definiton.ts file, they are stored in a 2d array for easier prototyping
 
+//cant construct on init, because the game is not yet initialized
 export class Dungeon {
 	number: number;
 	entrance: Position; // top left corner of the entrance
 	rooms: Room[];
+	dungeon_type: string;
 
-	//grids are stored in a sparse matrice
-	//0 = empty not used and not saved
-	//1 = occupied (by a room for example)
-	//2 = extendable (by a floor or room)
-	girds: number[][][] = [];
-
+	gird: Map<string, number> = new Map<string, number>();
+	surface_name: string;
 	entrance_is_generated: boolean = false;
+	first_room_is_generated: boolean = false;
 
-	constructor(entrance: Position) {
+	constructor(entrance: Position, dungeon_type: string) {
 		this.entrance = entrance;
 		this.rooms = [];
 
-		this.number = global[global.dungeons.length];
+		this.number = global.dungeons.length;
+		this.dungeon_type = dungeon_type;
+		this.surface_name = "dungeon_" + this.number;
+		this.generate_surface();
+	}
+
+	generate_surface() {
+		const surface = game.create_surface(this.surface_name);
+		surface.generate_with_lab_tiles = true;
+		surface.show_clouds = false;
+		surface.freeze_daytime = true;
+		surface.daytime = 0.01;
+		new OneWayTeleport(
+			"nauvis",
+			{
+				right_bottom: { x: this.entrance.x + 7, y: this.entrance.y + 7 } as Position,
+				left_top: { x: this.entrance.x + 3, y: this.entrance.y + 3 } as Position
+			} as BoundingBox,
+			this.surface_name,
+			{
+				x: 0,
+				y: 0
+			} as Position
+		);
 	}
 
 	get_surface() {
@@ -64,69 +86,106 @@ export class Dungeon {
 				entity.destroy({ raise_destroy: true, do_cliff_correction: true });
 			}
 		}
-
-		const surface = game.create_surface("dungeon_" + this.number);
-		surface.generate_with_lab_tiles = true;
-		surface.show_clouds = false;
-		surface.freeze_daytime = true;
-		surface.daytime = 0.01;
-		new OneWayTeleport(
-			"nauvis",
-			{
-				right_bottom: { x: this.entrance.x + 7, y: this.entrance.y + 7 } as Position,
-				left_top: { x: this.entrance.x + 3, y: this.entrance.y + 3 } as Position
-			} as BoundingBox,
-			"dungeon_" + this.number,
-			{
-				x: 0,
-				y: 0
-			} as Position
-		);
 	}
 
-	generate_room(surface: LuaSurface, roomtype: string, position: Position, orientation: number) {}
+	_noroom = [0, 2];
+	_room = [1, 10, 11];
+	//room
+	generate_room(roomtype: string, position: Position, orientation: number) {
+		const prototype = room_prototypes[roomtype];
+		const _layout = prototype.layout;
+		const layout = sparse_matrice_transformation(grid_to_sparse_matrice(_layout), orientation, { x: 1, y: 1 } as Position);
+
+		const tiles = [];
+		for (const [position, value] of layout) {
+			const x = Number(position.split("_")[0]);
+			const y = Number(position.split("_")[1]);
+
+			if (this._noroom.includes(value)) continue;
+			print("x: " + x + " y: " + y + " value: " + value);
+			//corners
+			tiles.push({
+				name: "concrete",
+				position: { x: x * 9, y: y * 9 }
+			});
+			tiles.push({
+				name: "concrete",
+				position: { x: x * 9 + 9, y: y * 9 }
+			});
+			tiles.push({
+				name: "concrete",
+				position: { x: x * 9, y: y * 9 + 9 }
+			});
+			tiles.push({
+				name: "concrete",
+				position: { x: x * 9 + 9, y: y * 9 + 9 }
+			});
+
+			//between corners (wall places)
+		}
+		print(this.get_surface().name);
+		this.get_surface().set_tiles(tiles);
+		print("roomtype: " + roomtype);
+	}
 }
 
-//nope copilot does weitd stuff
-function rotate_sparse_matrice(matrice: { x: number; y: number }[], orientation: number, center: Position) {
-	const new_matrice = [];
-	for (const position of matrice) {
-		let x = position.x;
-		let y = position.y;
+function sparse_matrice_transformation(matrice: Map<string, number>, rotation: number, center: Position): Map<string, number> {
+	let new_matrice = new Map<string, number>();
+	for (const [position, value] of matrice) {
+		let x = Number(position.split("_")[0]);
+		let y = Number(position.split("_")[1]);
 
-		for (let i = 0; i < orientation; i++) {
+		//centering
+		x -= center.x;
+		y -= center.y;
+
+		//rotation
+		for (let i = 0; i < rotation; i++) {
 			const old_x = x;
 			const old_y = y;
 			x = old_y;
 			y = -old_x;
 		}
-		new_matrice.push({ x: x + center.x, y: y + center.y });
+
+		//saving
+		new_matrice.set(position_to_string(x + center.x, y + center.y), value);
 	}
 	return new_matrice;
 }
-
 function grid_to_sparse_matrice(grid: number[][]) {
-	const matrice = [];
+	const matrice = new Map<string, number>();
 	for (let x = 0; x < grid.length; x++) {
 		for (let y = 0; y < grid[x].length; y++) {
 			if (grid[x][y] == 0) continue;
-			matrice.push({ x, y });
+			matrice.set(position_to_string(x, y), grid[x][y]);
 		}
 	}
 	return matrice;
+}
+
+function position_to_string(x: number, y: number): string {
+	return x + "_" + y;
 }
 
 function onTick(event: on_tick) {
 	//debug(game.get_player(1).position.x, game.get_player(1).position.y);
 }
 
-function onChunkGenerated(event: on_chunk_generated) {
+function on_chunk_charted(event: on_chunk_charted) {
+	const surface_name = game.surfaces[event.surface_index].name;
 	for (const dungeon of global.dungeons) {
-		if (!dungeon.entrance_is_generated) continue;
-		if (event.surface.name == "dungeon_" + dungeon.number) {
-		} //
-	}
+		if (surface_name == dungeon.surface_name) {
+			if (!dungeon.first_room_is_generated) {
+				const first_room = dungeon_prototypes[dungeon.dungeon_type].first_room;
 
+				dungeon.generate_room(first_room, { x: 0, y: 0 } as Position, 0);
+				dungeon.first_room_is_generated = true;
+			}
+		}
+	}
+}
+
+function onChunkGenerated(event: on_chunk_generated) {
 	if (event.surface.name != "nauvis") return;
 
 	const position = event.position;
@@ -143,16 +202,23 @@ function onChunkGenerated(event: on_chunk_generated) {
 	}
 }
 
+function on_init() {
+	global.dungeons = [];
+	const dungeon = new Dungeon({ x: 10, y: 10 }, "base_dungeon");
+	global.dungeons.push(dungeon);
+}
+
 export function DefineDungeon() {
 	DefineEvent(defines.events.on_chunk_generated, event => {
 		onChunkGenerated(event);
 	});
+	DefineEvent(defines.events.on_chunk_charted, event => {
+		on_chunk_charted(event);
+	});
 	DefineEvent(defines.events.on_tick, event => {
 		onTick(event);
 	});
-
-	global.dungeons = [];
-
-	const dungeon = new Dungeon({ x: 30, y: 30 });
-	global.dungeons.push(dungeon);
+	script.on_init(() => {
+		on_init();
+	});
 }
